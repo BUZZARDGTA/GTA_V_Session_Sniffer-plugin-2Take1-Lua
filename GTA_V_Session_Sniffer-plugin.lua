8efe8fd1a313b9a3ec456bc599401d50706f6ae2
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2024-07-08 02:03:46 +0200

    Update GTA_V_Session_Sniffer-plugin.lua

--- GTA_V_Session_Sniffer-plugin.lua
+++ GTA_V_Session_Sniffer-plugin.lua
@@ -2,7 +2,7 @@
 -- Description: Plugin for GTA V Session Sniffer project on GitHub.
 -- Allows you to have every usernames showing up on GTA V Session Sniffer.
 -- It logs every players from your sessions in "scripts/GTA_V_Session_Sniffer-plugin/log.txt".
--- If a  is met, automatically "Bail/Netsplit" you from the session.
+-- If a Fake Friend "Join Timeout" flagged user is met, automatically "Bail/Netsplit" you from the session.
 -- GitHub Repository: https://github.com/Illegal-Services/IP-Lookup-2Take1-Lua
 
 
